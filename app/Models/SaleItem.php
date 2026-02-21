b488951acfddc8a47367c985c010d1016ae3e90c
appligeeks <wslabn@gmail.com>
2026-02-21 13:17:10 -0500

    Fix: Add relationships to SaleItem model

--- app/Models/SaleItem.php
+++ app/Models/SaleItem.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class SaleItem extends Model
 {
-    //
+    protected $fillable = [
+        'sale_id',
+        'product_id',
+        'quantity',
+        'unit_price',
+        'subtotal',
+    ];
+
+    public function sale()
+    {
+        return $this->belongsTo(Sale::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
