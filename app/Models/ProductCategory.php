334312538cfe72d1340b2ea4d145fa1ffd62089d
appligeeks <wslabn@gmail.com>
2026-02-21 13:59:38 -0500

    Fix: Add products relationship to ProductCategory model

--- app/Models/ProductCategory.php
+++ app/Models/ProductCategory.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductCategory extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'name',
+        'description',
+        'is_system',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function products()
+    {
+        return $this->hasMany(Product::class, 'category_id');
+    }
 }
